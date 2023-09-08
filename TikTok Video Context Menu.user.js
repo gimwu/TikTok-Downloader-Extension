de9ed5d6ec8fcbf1f974a686809b9ab725da6384
吴隽卿 <wujunqing@limayao.com>
2023-09-08 16:55:35 +0800

    feat:download

--- TikTok Video Context Menu.user.js
+++ TikTok Video Context Menu.user.js
@@ -11,70 +11,102 @@
 (function () {
     'use strict';
 
-    window.addEventListener('load', () => {
-        console.log('Window loaded');
+    const HEADERS = new Headers({
+        'User-Agent': 'TikTok 26.2.0 rv:262018 (iPhone; iOS 14.4.2; en_US) Cronet'
+    });
 
-        const videoContainers = document.querySelectorAll('div[class*="DivVideoContainer"]');
+    const getIdVideo = (url) => {
+        const matching = url.includes("/video/");
+        if (!matching) {
+            console.error("[X] Error: URL not found");
+            return null;
+        }
+        const idVideo = url.substring(url.indexOf("/video/") + 7, url.length);
+        return (idVideo.length > 19) ? idVideo.substring(0, idVideo.indexOf("?")) : idVideo;
+    };
 
-        console.log('Video containers found:', videoContainers.length);
+    const getVideoNoWM = async (url) => {
+        const idVideo = getIdVideo(url);
+        if (!idVideo) return null;
 
+        const API_URL = `https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${idVideo}`;
+        try {
+            const request = await fetch(API_URL, { method: "GET", headers: HEADERS });
+            const body = await request.text();
+            const res = JSON.parse(body);
+            console.log(res)
+            return res;
+        } catch (err) {
+            console.error("Error:", err);
+            return null;
+        }
+    };
+
+    const addContextMenuOption = (videoContainers) => {
         videoContainers.forEach(videoContainer => {
             videoContainer.addEventListener('contextmenu', (e) => {
-                console.log('Context menu triggered on video container');
-
                 e.preventDefault();
-
-                // 创建一个新的菜单项
-                const menuItem = document.createElement('li');
-                menuItem.innerText = 'hello';
-                menuItem.style.cursor = 'pointer';
-
-                // 在一段时间后添加到弹出菜单中，以确保它已被创建
-                setTimeout(() => {
-                    const popupMenu = document.querySelector('ul[class*="UlPopupContainer"]');
-                    if (popupMenu) {
-                        console.log('Popup menu found', popupMenu);
-                        // 检查是否已经有一个"hello"选项
-                        const existingHelloItem = Array.from(popupMenu.querySelectorAll('li')).find(li => li.textContent === "hello");
-                        if (existingHelloItem) {
-                            console.log('"hello" option already exists');
-                            return;
-                        }
-                        // 获取菜单中的第一个li元素并复制它
-                        const firstMenuItem = popupMenu.querySelector('li');
-                        if (firstMenuItem) {
-                            const newItem = firstMenuItem.cloneNode(true);
-
-                            // 获取并更改文本span的内容
-                            const textSpan = newItem.querySelector('span[class*="SpanItemText"]');
-                            if (textSpan) {
-                                textSpan.textContent = "hello";
-                            }
-
-                            // 添加点击事件监听器
-                            newItem.addEventListener('click', () => {
-                                alert('hello, tiktok');
-                            });
-
-                            // 将新的li元素添加到菜单中
-                            popupMenu.appendChild(newItem);
-
-                            console.log('Added new menu item');
-                        } else {
-                            console.log('Failed to find the first menu item');
-                        }
-                    } else {
-                        console.log('Popup menu not found');
-                    }
-                }, 100);
-
-                menuItem.addEventListener('click', () => {
-                    console.log('Hello menu item clicked');
-                    alert('hello, tiktok');
-                    // 从菜单中移除自定义项
-                    menuItem.remove();
-                });
+                createAndAppendMenuItem(e);
             });
         });
+    };
+
+    const createAndAppendMenuItem = (e) => {
+        setTimeout(() => {
+            const popupMenu = document.querySelector('ul[class*="UlPopupContainer"]');
+            if (popupMenu) {
+                const existingHelloItem = Array.from(popupMenu.querySelectorAll('li')).find(li => li.textContent === "hello");
+                if (!existingHelloItem) {
+                    const firstMenuItem = popupMenu.querySelector('li');
+                    if (firstMenuItem) {
+                        const newItem = firstMenuItem.cloneNode(true);
+                        const textSpan = newItem.querySelector('span[class*="SpanItemText"]');
+                        if (textSpan) {
+                            textSpan.textContent = "hello";
+                        }
+
+                        newItem.addEventListener('click', async () => {
+                            const currentPageUrl = window.location.href;
+                            console.log('Current Page URL:', currentPageUrl);
+
+                            const videoData = await getVideoNoWM(currentPageUrl);
+                            if (videoData) {
+                                try {
+                                    const response = await fetch(videoData.aweme_list[0].video.play_addr.url_list[0]);
+                                    const videoBlob = await response.blob();
+                                    const blobUrl = URL.createObjectURL(videoBlob);
+
+                                    const a = document.createElement('a');
+                                    a.href = blobUrl;
+                                    a.download = videoData.aweme_list[0].aweme_id+".mp4"; // You can give any name you want
+                                    document.body.appendChild(a);
+                                    a.click();
+                                    document.body.removeChild(a);
+                                } catch (error) {
+                                    console.error('Error fetching video data:', error);
+                                }
+                            } else {
+                                console.error('Failed to fetch video data');
+                            }
+                        });
+
+                        popupMenu.appendChild(newItem);
+                    } else {
+                        console.log('Failed to find the first menu item');
+                    }
+                }
+            } else {
+                console.log('Popup menu not found');
+            }
+        }, 100);
+    };
+
+    window.addEventListener('load', () => {
+        console.log('Window loaded');
+        const videoContainers = document.querySelectorAll('div[class*="DivVideoContainer"]');
+        console.log('Video containers found:', videoContainers.length);
+
+        addContextMenuOption(videoContainers);
     });
+
 })();
